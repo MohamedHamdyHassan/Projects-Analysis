a0fecf15835e56e0bf5218adf27a92b64906c6ea
MohamedHamdyHassan <mohamed2hhamdy@gmail.com>
2023-12-13 19:34:58 +0200

    make cookicutter

---
+++ Bikes Analysis/Bikes Analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Make Analysis to find the best profit through years and seasons and Build a ML model to predict the profit in the next years ',
+    author='Mohamed Hamdy',
+    license='',
+)
